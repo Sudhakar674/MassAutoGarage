05bd0efae0e980707038f62afc670f5697227383
Sudhakar674 <sudhakar@massnexgen.com>
2024-11-21 19:41:24 +0530

    changes

--- MassAutoGarage/Controllers/HR_GeneralRequestController.cs
+++ MassAutoGarage/Controllers/HR_GeneralRequestController.cs
@@ -1,25 +1,118 @@
-﻿using System;
+﻿using MassAutoGarage.Data.HRMS_Loan;
+using MassAutoGarage.Data;
+using MassAutoGarage.Models.HRMS_Loan;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MassAutoGarage.Models.HR_GeneralRequest;
+using MassAutoGarage.Data.HR_GeneralRequest;
+using Microsoft.Azure.Pipelines.WebApi;
+using Org.BouncyCastle.Asn1.X509;
+using MassAutoGarage.Models.HRMS_EmployeeBankDetails;
+using System.Data;
+using MassAutoGarage.Models.HRMS_SalesTarget;
 
 namespace MassAutoGarage.Controllers
 {
     public class HR_GeneralRequestController : Controller
     {
         // GET: HR_GeneralRequest
-        public ActionResult GeneralRequest()
+        HR_GeneralRequestModel model = new HR_GeneralRequestModel();
+        HR_GeneralRequestDL DL = new HR_GeneralRequestDL();
+        ClsGeneral objcls = new ClsGeneral();
+        //public ActionResult GeneralRequest(HR_GeneralRequestModel model)
+        //{
+        //    ViewBag.ddlEmployee = DL.ddlEmployee();
+        //    ViewBag.ddlDesignation = DL.ddlDesignation();
+        //    ViewBag.ddlBranch = DL.ddlBranch();
+        //    ViewBag.ddlDepartment = DL.ddlDepartment();
+        //    ViewBag.SalaryCertificatelst = DL.SalaryCertificatelst();
+        //    return View();
+        //}
+
+        //public ActionResult GeneralRequest(HR_GeneralRequestModel model)
+        //{
+        //    ViewBag.ddlEmployee = DL.ddlEmployee();
+        //    ViewBag.ddlDesignation = DL.ddlDesignation();
+        //    ViewBag.ddlBranch = DL.ddlBranch();
+        //    ViewBag.ddlDepartment = DL.ddlDepartment();
+
+        //    // Binding dynamic checkbox values start..........
+        //    //IEnumerable<HR_GeneralRequestModel> dynamicDataSc = DL.SalaryCertificatelst();
+        //    //ViewBag.DynamicValuesSC = dynamicDataSc;
+
+        //    IEnumerable<HR_GeneralRequestModel> dynamicDataSc = DL.Certificatelst();
+        //    ViewBag.DynamicValuesSC = dynamicDataSc;
+
+        //    // Binding dynamic checkbox values end............
+
+        //    return View();
+        //}
+
+
+        public ActionResult GeneralRequest(HR_GeneralRequestModel model)
         {
-            return View();
+            List<HR_GeneralRequestModel> lst = new List<HR_GeneralRequestModel>();
+
+            ViewBag.ddlEmployee = DL.ddlEmployee();
+            ViewBag.ddlDesignation = DL.ddlDesignation();
+            ViewBag.ddlBranch = DL.ddlBranch();
+            ViewBag.ddlDepartment = DL.ddlDepartment();
+
+            var GroupList = DL.Certificatelst();
+            foreach (var item in GroupList)
+            {
+                HR_GeneralRequestModel obj = new HR_GeneralRequestModel();
+                obj.Id = item.Id;
+                obj.Certificate = item.Certificate;
+                lst.Add(obj);
+            }
+            model.lst = lst;
+            return View(model);
         }
+
+        public JsonResult SaveGeneralRequest(HR_GeneralRequestModel model)
+        {
+            try
+            {
+                model.CreatedBy = Session["userId"].ToString();
+                model.QueryType = "11";
+
+                if (model.SalaryCertificate == "on")
+                {
+                    model.SalaryCertificate = "1";
+                }
+                else
+                {
+                    model.SalaryCertificate = "0";
+                }
+
+                model = DL.AddUpdate(model);
+                if (model.Message == "1")
+                {
+                    model.Result = "yes";
+                }
+                else
+                {
+                    model.Result = "";
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
 
         public ActionResult GeneralRequestList()
         {
             return View();
         }
 
-       
+
 
 
 
---
+++ MassAutoGarage/Models/HR_GeneralRequest/HR_GeneralRequestModel.cs
@@ -0,0 +1,64 @@
+﻿using DocumentFormat.OpenXml.Bibliography;
+using MassAutoGarage.Models.HRMS_Loan;
+using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MassAutoGarage.Models.HR_GeneralRequest
+{
+    public class HR_GeneralRequestModel
+    {
+        public string Idincrept { get; set; }
+        public string Id { get; set; }
+        public string VoucherNumber { get; set; }
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Date { get; set; }
+        public string DesignationId { get; set; }
+        public string Designation { get; set; }
+        public string BranchId { get; set; }
+        public string BranchName { get; set; }
+        public string DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public string EmpId { get; set; }
+        public string SalaryCertificate { get; set; }
+        public string SalaryTransferLetter { get; set; }
+        public string NOC { get; set; }
+        public string ExperienceCertificate { get; set; }
+        public string BankEstablishment { get; set; }
+        public string AcctNoIBANNo { get; set; }
+        public string OpneNewBankAcct { get; set; }
+        public string TransferLoanToOtherBank { get; set; }
+        public string PersonalLoan { get; set; }
+        public string NOCToEmbassy { get; set; }
+        public string ConfirmationLettrToBank { get; set; }
+        public string LoanTopUp { get; set; }
+        public string Others { get; set; }
+        public string CashAdvance { get; set; }
+        public string InitialNewEmp { get; set; }
+        public string Against { get; set; }
+        public string BehalfAgainst { get; set; }
+        public string Reason { get; set; }
+        public string MonthlyDeduction { get; set; }
+        public string SalarySlips3Months { get; set; }
+        public string SalarySlips6Months { get; set; }
+        public string TrafficFineDeductionAmt { get; set; }
+        public string SalaryCard { get; set; }
+        public string EmpSignature { get; set; }
+        public string MobileNumber { get; set; }
+        public string CreatedBy { get; set; }
+        public string QueryType { get; set; }
+        public string Message { get; set; }
+        public string Result { get; set; }
+        public string Certificate { get; set; }
+        public string GeneralRequestId { get; set; }
+        public string CertificateDetails { get; set; }
+        public List<HR_GeneralRequestModel> HR_GeneralRequestModelList { get; set; }
+        public List<HR_GeneralRequestModel> lst { get; set; }
+
+
+    }
+}
